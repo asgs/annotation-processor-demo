4b08f7e28d3da27e44da0e2c376eaf8ddb244c95
asgs <asgosa.eeegct@gmail.com>
2017-08-30 15:45:49 +0530

    Configured sourcepath in pom.xml

--- src/main/java/org/asgs/apdemo/Main.java
+++ src/main/java/org/asgs/apdemo/Main.java
@@ -16,10 +16,12 @@ import javax.tools.ToolProvider;
  */
 public class Main {
   public static void main(String[] args) {
-    //SampleClassWithPrivateOnlyAnnotation klass = new SampleClassWithPrivateOnlyAnnotation("sdsd");
+    System.out.println("Sourcepath is " + args[0]);
+    File[] files1 = new File(new File(args[0]).getAbsolutePath()).listFiles(f -> f.getName().endsWith(".java"));
+    Arrays.asList(files1).stream().forEach(System.out::println);
     JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
     StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-    Iterable<? extends JavaFileObject> files = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(new File("./classes").listFiles(f -> f.getName().endsWith(".java"))));
+    Iterable<? extends JavaFileObject> files = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(files1));
     JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, null, null, files);
     task.setProcessors(Arrays.asList(new PrivateOnlyAnnotationProcessor()));
     System.out.println(task.call());
